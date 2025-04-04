d54afb8841d4cf5f26ae5cde299d395a8e4cb21d
T-Visor <turkimbro@yahoo.com>
2025-04-04 15:28:29 -0400

    improved map interface by letting it re-adjust when the sidebar is closed

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,12 @@
-"use client";
+"use client"
 
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { Sparkles, Search } from "lucide-react"
+import dynamic from "next/dynamic";
 
-const position: LatLngExpression = [51.505, -0.09];
+const MapWithSearch = dynamic(() => import("@/components/MapWithSearch"), {
+  ssr: false,
+  loading: () => <div className="text-white p-4">Loading map...</div>, // optional
+});
 
-const Map = () => {
-  return (
-    <div className="h-full w-full">
-      <div className="py-2 flex items-center space-x-3 px-4 h-13">
-        <SidebarTrigger />
-
-        {/* Vertical line separator*/}
-        <div className="h-6 w-px bg-gray-600" /> 
-
-        {/* Search bar with icon */}
-        <div className="relative w-full">
-          <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-gray-400" />
-          <Input placeholder="Search" className="pl-9"/>
-        </div>
-
-        {/* Button to execute search */}
-        <Button>
-          <Sparkles />
-        </Button>
-      </div>
-      <MapContainer
-        center={position}
-        zoom={13}
-        scrollWheelZoom={true}
-        zoomControl={false}
-        className="h-full w-full"
-        attributionControl={false}
-      >
-        <TileLayer
-          attribution=""
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+export default function Home() {
+  return <MapWithSearch />;
 }
-export default Map;
---
+++ components/MapWithSearch.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useEffect } from "react"
+import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Sparkles, Search } from "lucide-react"
+
+const position: LatLngExpression = [51.505, -0.09];
+
+const MapResizeHandler = () => {
+  const map = useMap();
+  const open = useSidebar();
+
+  useEffect(() => {
+    setTimeout(() => {
+      map.invalidateSize();
+    }, 200); // small delay gives layout time to settle
+  }, [open, map]);
+
+  return null;
+};
+
+const MapWithSearch = () => {
+  return (
+    <div className="h-full w-full">
+      <div className="py-2 flex items-center space-x-3 px-4 h-13">
+        {/* Button to control collapsible sidebar */}
+        <SidebarTrigger />
+
+        {/* Vertical line separator*/}
+        <div className="h-6 w-px bg-gray-600" /> 
+
+        {/* Search bar with icon */}
+        <div className="relative w-full">
+          <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-gray-400" />
+          <Input placeholder="Search" className="pl-9"/>
+        </div>
+
+        {/* Button to execute search */}
+        <Button>
+          <Sparkles />
+        </Button>
+      </div>
+
+      {/* Interactive Map*/}
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+        zoomControl={false}
+        className="h-full w-full"
+        attributionControl={false}
+      >
+        <MapResizeHandler />
+        <TileLayer
+          attribution=""
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
+export default MapWithSearch;
