193c75745286fbce2ba7b5de579020ef196bee89
Turhan Akira <turkimbro@yahoo.com>
2025-04-06 11:09:51 -0400

    added zustand state store for managing search results

---
+++ lib/searchResultsStore.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+
+const useSearchResultsStore = create((set) => ({
+  // Entire list of results
+  searchResults: [],
+  setSearchResults: (searchResults) => set({ searchResults }),
+
+  // Selected search result from list
+  searchResult: null,
+  setSearchResult: (searchResult) => set({ searchResult }),
+  unselectSearchResult: () => set({ searchResult: null }),
+}));
+
+export default useSearchResultsStore;
