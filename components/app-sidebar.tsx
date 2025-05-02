70636ca314e32db7d29b22b974f238756db445ce
Turhan Akira <turkimbro@yahoo.com>
2025-05-02 16:14:46 -0400

    refactored code for highlighting search results, now need to scroll to search result in sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -33,38 +33,43 @@ const AppSidebar = () => {
         <SidebarGroup>
           <SidebarGroupContent className="p-4 overflow-auto">
             <div className="grid grid-cols-1 gap-3">
-              {(searchResults.length > 0) && (searchResults.map((result) => (
-                <div
-                  key={result._id}
-                  className="w-full rounded-md overflow-hidden"
-                >
-                  <Button
-                    variant={isEqual(searchResult, [result._source.location.lat, result._source.location.lon]) ? "secondary" : "ghost"}
-                    onClick={() => setSearchResult([result._source.location.lat, result._source.location.lon])}
-                    className="w-full h-auto text-left flex flex-col items-start py-2 rounded-md hover:bg-gray-800 transition"
+              {(searchResults.length > 0) && (searchResults.map((result) => {
+                const coordinates: [number, number] = [result._source.location.lat, result._source.location.lon];
+
+                return (
+                  <div
+                    key={result._id}
+                    className="w-full rounded-md overflow-hidden"
                   >
-                    <div className="w-full flex flex-col">
-                      <span className="text-base font-medium text-white mb-1">
-                        {`${formatCurrency(result._source.price)}`}
-                      </span>
-                      <span className="text-sm text-gray-400 mb-0.5">
-                        {`${result._source.bedroom_number ?? 0} bed | 
+                    {/* Highlight this button if its coordinates match the currently selected location */}                    
+                    <Button
+                      variant={isEqual(searchResult, coordinates) ? "secondary" : "ghost"}
+                      onClick={() => setSearchResult(coordinates)}
+                      className="w-full h-auto text-left flex flex-col items-start py-2 rounded-md hover:bg-gray-800 transition"
+                    >
+                      <div className="w-full flex flex-col">
+                        <span className="text-base font-medium text-white mb-1">
+                          {`${formatCurrency(result._source.price)}`}
+                        </span>
+                        <span className="text-sm text-gray-400 mb-0.5">
+                          {`${result._source.bedroom_number ?? 0} bed | 
                           ${result._source.bathroom_number ?? 0} bath`}
-                      </span>
-                      <span className="text-sm text-gray-400 mb-0.5 truncate whitespace-pre-line">
-                        {result._source.address.replace(",", "\n")}
-                      </span>
-                      <a 
-                        className="text-sm text-blue-500"
-                        href={result._source.property_url}
-                        target="_blank" 
-                      >
-                        View Source
-                      </a>
-                    </div>
-                  </Button>
-                </div>
-              )))}
+                        </span>
+                        <span className="text-sm text-gray-400 mb-0.5 truncate whitespace-pre-line">
+                          {result._source.address.replace(",", "\n")}
+                        </span>
+                        <a
+                          className="text-sm text-blue-500"
+                          href={result._source.property_url}
+                          target="_blank"
+                        >
+                          View Source
+                        </a>
+                      </div>
+                    </Button>
+                  </div>
+                )
+              }))}
             </div>
           </SidebarGroupContent>
         </SidebarGroup>
